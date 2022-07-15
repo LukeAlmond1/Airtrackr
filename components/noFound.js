06c09c71744916bff61d3525fdf2c8b477505740
lukeAlmond1 <lukeaaron106@gmail.com>
2022-07-15 08:20:44 +0100

    changed next/image again

--- components/noFound.js
+++ components/noFound.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Image from "next/Image";
+import Image from "next/image";
 import fnd from "../media/fnd.svg";
 import nofnd from "../media/nofnd.svg";
 import far from "../media/far.svg";
